1759bddb9b28a3b2c269d612c13a3f43f4a37bc9
khimDev <ahmadrochim266@gmail.com>
2026-02-08 21:43:47 +0700

    nambah progres + kosa kata

--- fcd/introduction.js
+++ fcd/introduction.js
@@ -1,5 +1,29 @@
-let firtsName = "Rokhim";
-let LastName = "Dev";
+// let firtsName = "Rokhim";
+// let LastName = "Dev";
 
-let fullName = firtsName.concat(" ", LastName);
-console.log(fullName);
+// let fullName = firtsName.concat(" ", LastName);
+// console.log(fullName);
+
+// /* jas Bracket Notation */
+// let salam = "azizir";
+// let sal = salam[0] + salam[2];
+// console.log(sal);
+
+// /* another example */
+// let statement = "She said, \"Hello!\"";
+// console.log(statement);
+
+// /* Literals white multiple lines */
+// let poem = ` Roes is beautiful
+//  i hope she's got a
+//  perfect person in his life`;
+// console.log(poem);
+
+/* Complex Literals template */
+const song = "Bohemian Rhapsody";
+const score = 9.5;
+const highestScore = 10;
+const output = `One of my favorite songs is "${song}". I rated it ${
+  (score / highestScore) * 100
+}%.`;
+console.log(output); 
